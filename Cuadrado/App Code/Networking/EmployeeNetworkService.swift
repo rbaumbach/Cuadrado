ffed9a5517504562347ecc6e76824f0f778536c5
Ryan <github@ryan.codes>
2019-11-23 12:48:01 -0700

    Create FakeEmployeeNetworkService

--- Cuadrado/App Code/Networking/EmployeeNetworkService.swift
+++ Cuadrado/App Code/Networking/EmployeeNetworkService.swift
@@ -1,7 +1,11 @@
 import Foundation
 
+protocol EmployeeNetworkServiceProtocol {
+    func getEmployees(completionHandler: @escaping (Result<[Employee], APIClientError>) -> Void)
+}
+ 
 class EmployeeNetworkService {
-    func getEmployees(completionHandler: (Result<[Employee], APIClientError>) -> Void) {
+    func getEmployees(completionHandler: @escaping (Result<[Employee], APIClientError>) -> Void) {
         
     }
 }
